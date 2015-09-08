715f459558ba7e2fa7db79330657ed975c683619
Farengar <bbf3211@gmail.com>
2015-09-08 17:53:06 -0500

    Added winch

--- robot.py
+++ robot.py
@@ -12,6 +12,7 @@ class TestProgram(wpilib.SampleRobot):
 
     # Joystick channel
     joystickChannel = 0
+    
 
     def robotInit(self):
         '''Robot initialization function'''
@@ -25,9 +26,8 @@ class TestProgram(wpilib.SampleRobot):
 
         # invert left side motors
         self.robotDrive.setInvertedMotor(RobotDrive.MotorType.kFrontLeft, True)
-
-        # possibly needs changed
         self.robotDrive.setInvertedMotor(RobotDrive.MotorType.kRearLeft, True)
+        self.winch = wpilib.Talon(4)
 
         self.stick = wpilib.Joystick(self.joystickChannel)
 
@@ -40,6 +40,13 @@ class TestProgram(wpilib.SampleRobot):
             self.robotDrive.mecanumDrive_Cartesian(self.stick.getX(),
                                                   self.stick.getY(),
                                                   self.stick.getZ(), 0);
+            if self.stick.getRawButton(0):
+                self.winch.set(1)
+            elif self.stick.getRawButton(1):
+                self.winch.set(-1)
+            else:
+                self.winch.set(0)
+            
             wpilib.Timer.delay(0.005) # wait 5ms to avoid hogging CPU cycles
 
 def main():
